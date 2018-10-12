0cf50666b62bd3d7dd3a75f5ad4a5663f3701514
htunhtunhtet <ryanhhh91@gmail.com>
2018-10-12 18:08:38 +0800

    add Sliders

--- App.js
+++ App.js
@@ -1,13 +1,63 @@
 import React from 'react'
-import { View } from 'react-native'
+import {
+    View,
+    Text,
+    StyleSheet,
+    TouchableHighlight,
+    TouchableNativeFeedback,
+    TouchableOpacity,
+    TouchableWithoutFeedback,
+    Slider
+
+} from 'react-native'
 import AddEntry from './components/AddEntry'
 
 export default class App extends React.Component {
+    handlePress = () => {
+        alert('Hello')
+    }
+
     render() {
         return (
-            <View>
-                <AddEntry />
+            <View style={styles.container}>
+                <AddEntry/>
+                {/*<TouchableOpacity
+                    style={styles.btn} onPress={this.handlePress} underlayColor='#d4271b'>
+                    <Text style={styles.btnText}>Touchable Highlight</Text>
+                </TouchableOpacity>*/}
+
+                {/*<Slider
+                    minimumValue={-10}
+                    maximumValue={10}
+                    step = {1}
+                    value = {this.state.value}
+                    onValueChange={(value) => this.setState(()=>({value}))}
+                />
+
+                <Text>
+                    Value = {this.state.value}
+                </Text>*/}
+
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop:200,
+        padding:20
+    },
+    btn: {
+        backgroundColor:'#E53224',
+        padding:10,
+        paddingLeft:50,
+        paddingRight:50,
+        justifyContent:'center',
+        alignItems:'center',
+        borderRadius: 5
+    },
+    btnText: {
+        color: '#fff'
+    }
+})
