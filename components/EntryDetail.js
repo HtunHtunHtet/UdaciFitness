3354cf44e7b7fc07bb93119727778e162440698d
htunhtunhtet <ryanhhh91@gmail.com>
2018-11-02 14:58:47 +0800

    add entires

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -1,16 +1,69 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
-
+import { View, Text,StyleSheet } from 'react-native'
+import { connect } from 'react-redux'
+import { white } from '../utils/colors'
+import MetricCard from './MetricCard'
+import { addEntry } from "../actions";
+import { removeEntry } from "../utils/api";
+import {timeToString , getDailyReminderValue} from "../utils/helpers";
 
 class EntryDetail extends Component {
+
+    static navigationOption = ({ navigation }) => {
+        const {entryId} = navigation.state.params
+
+        const year = entryId.slice(0,4)
+        const month= entryId.slice(5,7)
+        const day = entryId.slice(8)
+
+
+        return {
+            title: `${month}/${day}/${year}`
+        }
+    }
+
     render(){
+
+        const {metrics} = this.props
+
         return(
-            <View>
+            <View style={styles.container}>
+                <MetricCard metrics ={metrics}/>
                 <Text>Entry Details - {this.props.navigation.state.params.entryId}</Text>
             </View>
         )
     }
-
 }
 
-export default EntryDetail
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: white,
+        padding: 15,
+    },
+})
+
+
+function mapStateToProps(state ,  { navigation }){
+    const {entryId} = navigation.state.params
+
+    return {
+        entryId,
+        metrics: state[entryId]
+    }
+}
+
+function mapDispatchToProps (dispatch, { navigation }){
+    const { entryId } = navigation.state.params
+
+    return {
+        remove: () => dispatch(addEntry({
+            [entryId]: timeToString() === entryId
+                        ?getDailyReminderValue()
+                        : null
+        })),
+        goback: () => navigation.goback()
+    }
+}
+
+export default connect(mapStateToProps)(EntryDetail)
