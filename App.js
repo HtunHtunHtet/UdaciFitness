e41e161f83f9590154665d91ee2259bd1dcaafb2
htunhtunhtet <ryanhhh91@gmail.com>
2018-11-02 15:53:19 +0800

    Live Feed

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import { purple,white } from './utils/colors'
 import { FontAwesome, Ionicons } from '@expo/vector-icons'
 import { Constants } from 'expo'
 import EntryDetail from './components/EntryDetail'
+import Live from './components/Live'
 
 
 function UdaciStatusBar({ backgroundColor, ...props }) {
@@ -35,6 +36,14 @@ const Tabs = TabNavigator({
         navigationOptions:{
             tabBarLabel: 'Add Entry',
             tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
+        }
+    },
+
+    Live: {
+        screen: Live,
+        navigationOptions: {
+            tabBarLabel: 'Live',
+            tabBarIcon: ({ tintColor }) => <FontAwesome name='ios-speedometer' size={30} color={tintColor} />
         }
     }
 
